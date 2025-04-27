69df2e6cde602ff159148ad0aa362c949ff47dd7
Knoll <cangzihan@gmail.com>
2025-04-27 20:24:44 +0900

    Automatically display analysis of answered questions

--- book_list.js
+++ book_list.js
@@ -3,5 +3,10 @@ const bookList = [
     Title: "蓝宝书N3文法",
     Path: "BlueBook_N3.js",
     DataVar: "bluebook_n3"
+  },
+  {
+    Title: "JLPT-N3-GPT版",
+    Path: "GPT_N3.js",
+    DataVar: "gpt_n3"
   }
 ];
