560b63f79e2a3e6339284bdbf3788ede62dcf4f4
Knoll <cangzihan@gmail.com>
2025-07-26 18:53:00 +0900

    test table format data

--- auto_gana.py
+++ auto_gana.py
@@ -3,6 +3,50 @@
 import pykakasi
 import os
 
+kana_description_dict = {
+    "アイディア": "idea",
+    "アクセシビリティ": "accessibility",
+    "アノテーション": "annotation",
+    "アルゴリズム": "algorithm",
+    "ガイドライン": "guidelines",
+    "クラウドサービス": "Cloud Services",
+    "グラフ": "graph",
+    "コーポレートガバナンス": "Corporate Governance",
+    "サイト": "site",
+    "サービス": "service",
+    "サービスマネジメント": "Service Management",
+    "サービスマーク": "Service Mark",
+    "ソースプログラム": "源程序",
+    "システム": "system",
+    "スマートファクトリー": "Smart Factory",
+    "セキュリティ": "安全功能",
+    "セキュリティマネジメントシステム": "Security Management System",
+    "ソフトウェアライフサイクル": "Software Life Cycle",
+    "ソリューション": "solution",
+    "データ": "data",
+    "ツール": "tool",
+    "メール": "mail",
+    "メールサーバ": "Mail Server",
+    "ハッカソン": "黑客马拉松",
+    "バイアス": "bias",
+    "パスワード": "密码",
+    "バリュー": "value",
+    "ビジョン": "vision",
+    "ビジネスモデル": "Business Model",
+    "プロトコル": "protocol, 协议",
+    "ブログ": "blog",
+    "プロセス": "process",
+    "プロセスマイニング": "Process Mining",
+    "ブロックチェーン": "区块链",
+    "ベンダー": "供应商，卖方",
+    "ポイント": "point",
+    "マニュアル": "manual",
+    "マルウェア": "恶意软件",
+    "マーケティング": "营销",
+    "ミッション": "mission",
+    "レベル": "level"
+}
+
 # 初始化 pykakasi 用于日语文本转换
 kks = pykakasi.kakasi()
 
@@ -27,7 +71,14 @@ def process_jp_sentence(text):
     annotated_text = ''
     for item in result:
         if item['orig'] != item['hira']:
-            annotated_text += f"[{item['orig']}]" + "{" + f"{item['hira']}" + "}"
+            description = item['hira']
+
+            # 根据字典解析一些常见片假名
+            if item['orig'] == item['kana']:
+                if item['kana'] in kana_description_dict:
+                    description = kana_description_dict[item['kana']]
+
+            annotated_text += f"[{item['orig']}]" + "{" + f"{description}" + "}"
         else:
             # 假名或无需注释的部分保持原样
             annotated_text += item['orig']
@@ -40,6 +91,9 @@ def test():
     annotated_text = process_jp_sentence(text)
     print(annotated_text)
     text = "彼は[東京大学]{とうきょうだいがく}で勉強している。"
+    annotated_text = process_jp_sentence(text)
+    print(annotated_text)
+    text = "4月1日から5月31日までに、取引①から取引⑤があった。各取引の受注、売上計上,現金回収の状況が表のとおりであるとき、この取引先に対する5月31日時点の売掛金は何万円か。ここで、4月1日時点で売掛金残高はないものとする。\n<table border=\"1\" style=\"width: 100%; table-layout: fixed;\">\n<thead>\n  <tr>\n    <th style=\"width: 15%;\">取引</th>\n    <th style=\"width: 15%;\">日付</th>\n    <th style=\"width: 20%;\">取引内訳</th>\n    <th style=\"width: 20%;\">金額(万円)</th>\n    <th style=\"width: 30%;\">備考</th>\n  </tr>\n</thead>\n<tbody>\n  <tr>\n    <td style=\"width: 15%; text-align: center;\">取引1</td>\n    <td style=\"width: 15%;\">4月2日<br>4月10日<br>4月30日</td>\n    <td style=\"width: 20%;\">受注<br>売上計上<br>現金回收</td>\n    <td style=\"width: 20%; text-align: right;\">800<br>800<br>800</td>\n    <td style=\"width: 30%;\"></td>\n  </tr>\n  <tr>\n    <td style=\"width: 15%; text-align: center;\">取引2</td>\n    <td style=\"width: 15%;\">4月5日<br>4月15日<br>4月30日</td>\n    <td style=\"width: 20%;\">受注<br>売上計上<br>現金回收</td>\n    <td style=\"width: 20%; text-align: right;\">500<br>500<br>500</td>\n    <td style=\"width: 30%;\"></td>\n  </tr>\n  <tr>\n    <td style=\"width: 15%; text-align: center;\">取引3</td>\n    <td style=\"width: 15%;\">5月1日<br>5月10日</td>\n    <td style=\"width: 20%;\">受注<br>売上計上</td>\n    <td style=\"width: 20%; text-align: right;\">1300<br>1300</td>\n    <td style=\"width: 30%;\">6月30日<br>回収予定</td>\n  </tr>\n  <tr>\n    <td style=\"width: 15%; text-align: center;\">取引4</td>\n    <td style=\"width: 15%;\">5月6日<br>5月15日</td>\n    <td style=\"width: 20%;\">受注<br>売上計上</td>\n    <td style=\"width: 20%; text-align: right;\">1000<br>1000</td>\n    <td style=\"width: 30%;\">回収予定日未定</td>\n  </tr>\n  <tr>\n    <td style=\"width: 15%; text-align: center;\">取引5</td>\n    <td style=\"width: 15%;\">5月20日</td>\n    <td style=\"width: 20%;\">受注</td>\n    <td style=\"width: 20%; text-align: right;\">400</td>\n    <td style=\"width: 30%;\"></td>\n  </tr>\n</tbody>\n</table>\n"
     annotated_text = process_jp_sentence(text)
     print(annotated_text)
 
@@ -57,7 +111,10 @@ def process_file(fname):
     with open(f"{fname}_temp.js", 'w+', encoding='utf-8') as f:
         for line in content:
             if "Question" in line:
-                question = line.split('"')[3]
+                if len(line.split('"')) > 5:
+                    question = '"'.join(line.split('"')[3:-1])
+                else:
+                    question = line.split('"')[3]
                 annotated_question = process_jp_sentence(question)
                 f.write(f"      \"Question\": \"{annotated_question}\",\n")
             else:
